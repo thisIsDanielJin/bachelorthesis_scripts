4cdab132b7c1f179266b3b2d0d4cd49c91c7bbb5
I750579 <jin.wodke@sap.com>
2025-06-26 13:59:06 +0200

    working tundra setup

--- tundra-script.bash
+++ tundra-script.bash
@@ -56,47 +56,75 @@ sudo ip netns exec "$NS_TUNDRA" ip -6 route add default via "$LINK_LOCAL_HOST" d
 
 ### Step 2 - Setup Tundra
 
-# 2.0 enable ipv6 forwarding in the namespace
-sudo ip netns exec "$NS_TUNDRA" sysctl -w net.ipv6.conf.all.forwarding=1
+# constants
+TUNDRA_CONF="/etc/tundra-clat.conf"
+CLAT_IPV4="192.0.0.2"
+CLAT_IPV6="2a05:d014:144f:5f00:20d2::201"
+ROUTER_IPV4="192.0.0.1"
+ROUTER_IPV6="2a05:d014:144f:5f00:20d2::202"
+TUN_INTERFACE="clat"
 
-# 2.1 clone the repository
-git clone https://github.com/vitlabuda/tundra-nat64.git
-cd tundra-nat64
 
-# 2.2 build the project
+# 2.0 build tundra
+# 2.0.1 clone the repository
+#git clone https://github.com/vitlabuda/tundra-nat64.git
+cd /home/ubuntu/tundra-nat64
+# 2.0.2 build the project
 sudo apt install cmake
 CC=gcc cmake -S. -Bbuild
 make -Cbuild
+cd /home/ubuntu/
 
-# 2.3 install the project
-sudo cmake --install build
+# 2.1 write tundra configuration file
+sudo tee "$TUNDRA_CONF" > /dev/null <<EOF
+program.translator_threads = 4
+program.privilege_drop_user = 
+program.privilege_drop_group = 
 
-# 2.4 copy executable to /usr/local/sbin
-sudo cp build/tundra-nat64 /usr/local/sbin/
+io.mode = tun
+io.tun.device_path = /dev/net/tun
+io.tun.interface_name = $TUN_INTERFACE
+io.tun.owner_user = 
+io.tun.owner_group = 
+io.tun.multi_queue = no
 
-# 2.4.1 set the right ownership 
-sudo chown root:root /usr/local/sbin/tundra-nat64
-sudo chmod 755 /usr/local/sbin/tundra-nat64
+router.ipv4 = $ROUTER_IPV4
+router.ipv6 = $ROUTER_IPV6
+router.generated_packet_ttl = 224
 
-# 2.5.1 create the configuration directory
-sudo mkdir -p /usr/local/etc/tundra-clat
-cd ..
-# 2.5.2 copy tundra configuration file 
-sudo cp tundra-nat64/config_examples/debian_clat/tundra-clat.conf /usr/local/etc/tundra-clat/
+addressing.mode = clat
+addressing.nat64_clat.ipv4 = $CLAT_IPV4
+addressing.nat64_clat.ipv6 = $CLAT_IPV6
+addressing.nat64_clat_siit.prefix = 64:ff9b::
+addressing.nat64_clat_siit.allow_translation_of_private_ips = yes
 
-# 2.5.3 copy the startup and stop script
-sudo cp tundra-nat64/config_examples/debian_clat/start-tundra.sh  /usr/local/etc/tundra-clat/
-sudo cp tundra-nat64/config_examples/debian_clat/stop-tundra.sh  /usr/local/etc/tundra-clat/
+translator.ipv4.outbound_mtu = 1500
+translator.ipv6.outbound_mtu = 1500
 
-# 2.5.4 copy the systemd service file
-sudo cp tundra-nat64/config_examples/debian_clat/tundra-clat.service /etc/systemd/system/
+translator.6to4.copy_dscp_and_ecn = yes
+translator.4to6.copy_dscp_and_ecn = yes
+EOF
 
-# 2.5.5 reload the systemd daemon
-sudo systemctl daemon-reload
+# 2.2 create the TUNDRA device (nat64) inside the namespace
+sudo ip netns exec "$NS_TUNDRA" /home/ubuntu/tundra-nat64/build/tundra-nat64 --config-file="$TUNDRA_CONF" mktun
 
-# 2.5.6 start the service
-sudo systemctl start tundra-clat
+# 2.3 bring up CLAT device 
+sudo ip netns exec "$NS_TUNDRA" ip link set "$TUN_INTERFACE" up
 
+# 2.4 assign ip address to the clat device inside the namespace
+sudo ip netns exec "$NS_TUNDRA" ip addr add "$CLAT_IPV4"/32 dev "$TUN_INTERFACE"
 
+# 2.5 add route to the CLAT device (for ipv6 traffic)
+sudo ip netns exec "$NS_TUNDRA" ip -6 route add "$CLAT_IPV6" dev "$TUN_INTERFACE"
 
+# 2.6 add route to the CLAT device (for ipv4 traffic)
+sudo ip netns exec "$NS_TUNDRA" ip route add default via "$CLAT_IPV4" dev "$TUN_INTERFACE"
 
+# 2.7 add default ipv4 route to the namespace (for ipv4 traffic)
+sudo ip netns exec "$NS_TUNDRA" ip route add default via "$CLAT_IPV4" dev "$TUN_INTERFACE"
+
+# 2.8 enable ipv6 forwarding inside the namespace
+sudo ip netns exec "$NS_TUNDRA" sysctl -w net.ipv6.conf.all.forwarding=1
+
+# 2.9 start tundra
+sudo ip netns exec "$NS_TUNDRA" /home/ubuntu/tundra-nat64/build/tundra-nat64 --config-file="$TUNDRA_CONF"
